1a9094b053c59718109a8197b169909f1bffb513
Jady Godoi <jady.godoi@oi.net.br>
2025-01-06 17:06:52 -0300

    feat: manipulando json - simulando um servidor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListarComponent } from './pages/listar/listar.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ManipulandoJsonComponent } from './pages/manipulando-json/manipulando-json.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'listar', component: ListarComponent},
   {path: 'cadastro', component: CadastroComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'json', component: ManipulandoJsonComponent}
 ];
---
+++ src/app/pages/manipulando-json/manipulando-json.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import studentsData from '../../students.json';
+
+interface Student {
+  id: Number;
+  name: String;
+  email: String;
+  gender: String;
+}
+
+@Component({
+  selector: 'app-manipulando-json',
+  imports: [],
+  templateUrl: './manipulando-json.component.html',
+  styleUrl: './manipulando-json.component.css'
+})
+export class ManipulandoJsonComponent {
+  students: Student[] = studentsData;
+  constructor(){}
+
+  ngOnInit():void{
+    console.log(this.students)
+  }
+}
